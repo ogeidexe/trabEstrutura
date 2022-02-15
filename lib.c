d8c8a6f67f1fcd80c0c96974afc0a43741b4cf06
JefersonSSB <jefersonsilvasatosb@gmail.com>
2022-02-15 19:57:51 -0300

    Delete implementado

--- lib.c
+++ lib.c
@@ -1,100 +1,180 @@
 #include <stdio.h>
 #include <stdlib.h>
 /******************
-*Jeferson Silva 
-*Diego teixeira
-*
-*******************/
+ *Jeferson Silva
+ *Diego teixeira
+ *
+ *******************/
 //---------------------------Data structure--------------------//
 
 typedef struct player
 {
     char *name;
     int coin;
-}player;
-
+} player;
 
 typedef struct node
-{   
+{
     struct player player;
     int position;
     struct node *left;
     struct node *right;
-}node;
+} node;
 
 //---------------------------- Functions ----------------------//
 
-
 // Defalt Binarye Tree operatios
 node insertNode(node **root, player player); // insert ordering nodes OK
-node deleteNode(player player, node *root); // Delete nodes on tree diferent cases 
-node *searchNode(int moedas, node *root); // izi dude OK
+void deleteNode(node **root, player player); // Delete nodes on tree diferent cases
+node *searchNode(int moedas, node *root);    // izi dude OK
 
 int swapNodes(node whereNode, node fromNode); /// BOSS****
 
 // Custom Operatios
-int sumCoins(char* playerName,int coinsToSum);
-int rmCoins(char* playerName, int coinsToRemove);
-void printTree(node *root);//  mostrar arvore em ERD 
+int sumCoins(char *playerName, int coinsToSum);
+int rmCoins(char *playerName, int coinsToRemove);
+void printTree(node *root); //  mostrar arvore em ERD
 node *calcPositions(node *root);
 
+// Implementatios
 
-//Implementatios
+node insertNode(node **root, player player)
+{
 
-node insertNode(node **root,player player){
-    
     node *temp = NULL;
-    if(*root == NULL){        
-        temp  = (node *)malloc(sizeof(struct node));
+    if (*root == NULL)
+    {
+        temp = (node *)malloc(sizeof(struct node));
         temp->left = temp->right = NULL;
         temp->position = 1;
         temp->player = player;
-        *root =  temp;
-    }else  if((*root)->player.coin < player.coin){
+        *root = temp;
+    }
+    else if ((*root)->player.coin < player.coin)
+    {
         insertNode(&(*root)->left, player);
-    }else  if((*root)->player.coin > player.coin){
-        insertNode(&(*root)->right,player);
+    }
+    else if ((*root)->player.coin > player.coin)
+    {
+        insertNode(&(*root)->right, player);
     }
 }
 
-node deleteNode(player player, node *root){
-
-}
-
-node *searchNode(int moedas , node *root){
-    if(root != NULL) {
-        if(root->player.coin ==  moedas){
+node *searchNode(int moedas, node *root)
+{
+    if (root != NULL)
+    {
+        if (root->player.coin == moedas)
+        {
             return root;
-        }else if(root->player.coin <  moedas){
-           return searchNode(moedas, root->left);
-        }else if(root->player.coin >  moedas){
-            return searchNode(moedas,root->right);
+        }
+        else if (root->player.coin < moedas)
+        {
+            return searchNode(moedas, root->left);
+        }
+        else if (root->player.coin > moedas)
+        {
+            return searchNode(moedas, root->right);
         }
     }
 }
 
-void printTree (node *root) {
-   if (root != NULL) {
-		printTree (root->left);  
-		printf("**********    Player   *******\n");
-		printf ("Posicao: %d\n", root->position);
-		printf ("Nome: %s\n", root->player.name); 
-		printf ("Pontos: %d\n", root->player.coin);
-		printf("\n****************************\n");  
-		printTree (root->right);     
-   }
+void printTree(node *root)
+{
+    if (root != NULL)
+    {
+        printTree(root->left);
+        printf("**********    Player   *******\n");
+        printf("Posicao: %d\n", root->position);
+        printf("Nome: %s\n", root->player.name);
+        printf("Pontos: %d\n", root->player.coin);
+        printf("\n****************************\n");
+        printTree(root->right);
+    }
 }
 
-node *calcPositions(node *root ){
-    if(root == NULL) {
+node *calcPositions(node *root)
+{
+    if (root == NULL)
+    {
         root->position++;
     }
-	else if (root->left != NULL) {
-	   calcPositions(root->left);
-   }else if(root->right != NULL){
-       calcPositions(root->right);
-   }
-   
-   return root; 	
+    else if (root->left != NULL)
+    {
+        calcPositions(root->left);
+    }
+    else if (root->right != NULL)
+    {
+        calcPositions(root->right);
+    }
+
+    return root;
 }
 
+node *MaiorDireita(node **root)
+{
+    if ((*root)->right != NULL)
+        return MaiorDireita(&(*root)->right);
+    else
+    {
+        node *aux = *root;
+        if ((*root)->left != NULL) // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da esquerda!
+            *root = (*root)->left;
+        else
+            *root = NULL;
+        return aux;
+    }
+}
+
+void deleteNode(node **root, player player)
+{
+
+    if (*root == NULL)
+    { // esta verificacao serve para caso o numero nao exista na arvore.
+        printf("Player nao existe na arvore!");
+        return;
+    }
+    if (player.coin > (*root)->player.coin)
+        deleteNode(&(*root)->left, player);
+    else if (player.coin < (*root)->player.coin)
+        deleteNode(&(*root)->right, player);
+    else
+    { // se nao eh menor nem maior, logo, eh o player que estou procurando! :)
+        node *pAux = *root;
+        if (((*root)->left == NULL) && ((*root)->right == NULL))
+        { // se nao houver filhos...
+            free(pAux);
+            (*root) = NULL;
+        }
+        else
+        { // so tem o filho da direita
+            if ((*root)->left == NULL)
+            {
+                (*root) = (*root)->right;
+                pAux->right = NULL;
+                free(pAux);
+                pAux = NULL;
+            }
+            else
+            { // so tem filho da esquerda
+                if ((*root)->right == NULL)
+                {
+                    (*root) = (*root)->left;
+                    pAux->left = NULL;
+                    free(pAux);
+                    pAux = NULL;
+                }
+                else
+                {
+                    pAux = MaiorDireita(&(*root)->left);
+                    pAux->left = (*root)->left;
+                    pAux->right = (*root)->right;
+                    (*root)->left = (*root)->right = NULL;
+                    free((*root));
+                    *root = pAux;
+                    pAux = NULL;
+                }
+            }
+        }
+    }
+}
--- trabalho.c
+++ trabalho.c
@@ -1,38 +1,49 @@
 #include "lib.c"
 
 /******************
-*Jeferson Silva 
-*Diego Teixeira
-*
-*******************/
+ *Jeferson Silva
+ *Diego Teixeira
+ *
+ *******************/
 
 int main(int argc, char const *argv[])
 {
-    node *root  = NULL;  
+    node *root = NULL;
 
     player player1;
     player1.name = "jeronimo";
     player1.coin = 100;
-    insertNode(&root,player1);
+    insertNode(&root, player1);
 
     player player2;
     player2.name = "JOAO";
     player2.coin = 101;
-    insertNode(&root,player2);
+    insertNode(&root, player2);
 
     player player3;
     player3.name = "medonho";
     player3.coin = 99;
-    insertNode(&root,player3);
+    insertNode(&root, player3);
+
+    player player4;
+    player4.name = "Jefim";
+    player4.coin = 200;
+    insertNode(&root, player4);
+
+    player player5;
+    player5.name = "Diego";
+    player5.coin = 20;
+    insertNode(&root, player5);
 
     // printf("%s\n",root->player.name);
     // printf("%s\n",root->left->player.name);
     // printf("%s\n",root->right->player.name);
 
-    calcPositions(root);
+    root = calcPositions(root);
+    deleteNode(&root, player4);
     printTree(root);
-    node *temp=(node *) malloc (sizeof(struct node));
-    temp = searchNode(99,root);
-    //printf("Fulano encontrado: %s\n", temp->player.name);
+    node *temp = (node *)malloc(sizeof(struct node));
+    temp = searchNode(99, root);
+    // printf("Fulano encontrado: %s\n", temp->player.name);
     return 0;
 }
