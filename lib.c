6db812cdc920c3cc580f096800eaedfa28da163d
Diego Teixeira <ogeidexe@gmail.com>
2022-02-15 19:58:42 -0300

    nin

--- lib.c
+++ lib.c
@@ -7,16 +7,16 @@
 *******************/
 //---------------------------Data structure--------------------//
 
-typedef struct player
+typedef struct Player
 {
     char *name;
     int coin;
-}player;
+}Player;
 
 
 typedef struct node
 {   
-    struct player player;
+    struct Player player;
     int position;
     struct node *left;
     struct node *right;
@@ -26,8 +26,8 @@ typedef struct node
 
 
 // Defalt Binarye Tree operatios
-node insertNode(node **root, player player); // insert ordering nodes OK
-node deleteNode(player player, node *root); // Delete nodes on tree diferent cases 
+node insertNode(node **root, Player player); // insert ordering nodes OK
+node deleteNode(Player player, node **root); // Delete nodes on tree diferent cases 
 node *searchNode(int moedas, node *root); // izi dude OK
 
 int swapNodes(node whereNode, node fromNode); /// BOSS****
@@ -37,16 +37,17 @@ int sumCoins(char* playerName,int coinsToSum);
 int rmCoins(char* playerName, int coinsToRemove);
 void printTree(node *root);//  mostrar arvore em ERD 
 node *calcPositions(node *root);
+node *obteMenorNo(node *no);
 
 
 //Implementatios
 
-node insertNode(node **root,player player){
-    
-    node *temp = NULL;
+node insertNode(node **root,Player player){
     if(*root == NULL){        
+        node *temp = NULL;
         temp  = (node *)malloc(sizeof(struct node));
-        temp->left = temp->right = NULL;
+        temp->left = NULL; 
+        temp->right = NULL;
         temp->position = 1;
         temp->player = player;
         *root =  temp;
@@ -57,7 +58,54 @@ node insertNode(node **root,player player){
     }
 }
 
-node deleteNode(player player, node *root){
+node deleteNode(Player player, node **root){
+    node *parent = NULL;
+    node *current = *root;
+    //numero de moedas
+    parent  = searchNode(player.coin,*root);
+    //Caso  1 onde o no não tem filhos
+    //teste da arvore
+    if (current == NULL) {
+        return;
+    }  
+
+    if(current->left  ==  NULL && current->right ==  NULL){
+        printf(">%s:%s<",parent->player.name,(*root)->player.name);
+        if(current != *root){
+            if(parent->left == current)
+                parent->left =  NULL;
+            else
+                parent->right = NULL;
+        }else{
+            root = NULL;
+        }
+        free(current);  
+    //Caso  2 onde o no tem dois filhos
+    }else if(current->left && current->right){
+        // encontrar o sucessor na ordem -
+        node *sucessor =  obteMenorNo(current->right);
+        // guardar o valor do sucessor
+        Player playerTmp = sucessor->player;
+        //remove sucessor da arvore   
+        deleteNode(sucessor->player, root);
+        
+        current->player =  playerTmp;
+    //Caso 3 o no tem apenas um filho
+    }else{
+        node*  chlid =  (current->left) ? current->left : current->right;
+        //Se o no não for o no root defina o como um de seus filhos
+        if(current != *root){
+            if (current == parent->left){
+                parent->left =  chlid;
+            }else{
+                parent->right=  chlid;
+            }
+
+        }else{
+            *root =  chlid;
+        }
+        free(current);
+    }
 
 }
 
@@ -81,6 +129,7 @@ void printTree (node *root) {
 		printf ("Nome: %s\n", root->player.name); 
 		printf ("Pontos: %d\n", root->player.coin);
 		printf("\n****************************\n");  
+        
 		printTree (root->right);     
    }
 }
@@ -98,3 +147,10 @@ node *calcPositions(node *root ){
    return root; 	
 }
 
+node *obteMenorNo(node * no){
+    while (no->left != NULL)
+    {
+        no = no->left;
+    }
+    return no;
+} 
--- trabalho.c
+++ trabalho.c
@@ -10,17 +10,17 @@ int main(int argc, char const *argv[])
 {
     node *root  = NULL;  
 
-    player player1;
+    Player player1;
     player1.name = "jeronimo";
     player1.coin = 100;
     insertNode(&root,player1);
 
-    player player2;
+    Player player2;
     player2.name = "JOAO";
     player2.coin = 101;
     insertNode(&root,player2);
 
-    player player3;
+    Player player3;
     player3.name = "medonho";
     player3.coin = 99;
     insertNode(&root,player3);
@@ -29,10 +29,11 @@ int main(int argc, char const *argv[])
     // printf("%s\n",root->left->player.name);
     // printf("%s\n",root->right->player.name);
 
-    calcPositions(root);
+    //calcPositions(root);
     printTree(root);
+    deleteNode(player3, &root);
     node *temp=(node *) malloc (sizeof(struct node));
     temp = searchNode(99,root);
-    //printf("Fulano encontrado: %s\n", temp->player.name);
+    printf("Fulano encontrado: %s\n", temp->player.name);
     return 0;
 }
